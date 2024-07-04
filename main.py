c63bea2e8692e4425e9ab54ddcf3e731ad220021
SainaAntonova <163343511+SainaAntonova@users.noreply.github.com>
2024-07-04 22:48:07 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import os
 import asyncio
 import logging
+from dotenv import load_dotenv
+from aiogram.utils import executor
 from aiogram import Bot, Dispatcher
 from app.handlers import dp  # Импорт диспетчера из handlers
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-import os
-from dotenv import load_dotenv
+
 
 # Загрузка переменных окружения
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+if not BOT_TOKEN:
+    raise ValueError('No BOT_TOKEN provided')
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
@@ -20,7 +24,8 @@ async def main():
         logging.exception(f"Error in polling: {e}")
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)  # Set logging level to INFO
+    # executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO)  # Настройка логирования
     try:
         asyncio.run(main())
         print('Bot started.')
